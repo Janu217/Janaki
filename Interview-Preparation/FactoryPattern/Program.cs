19a9b8ab36fded794f91c53bd294cecb05c82b29
Janu217 <54596655+Janu217@users.noreply.github.com>
2020-03-28 23:19:18 +0530

    Add files via upload

---
+++ Interview-Preparation/FactoryPattern/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// //Not implemented factory pattern sample code.
+        //static void Main()
+        //{
+
+        //    IcomputerManufacture compmanufacture = null;
+        //    string comptype = "Desktop"; // or laptop
+        //        if (comptype == "Desktop")
+        //        {
+        //            compmanufacture = new desktop();
+        //        }
+        //        else if (comptype == "Laptop")
+        //        {
+        //            compmanufacture = new laptop();
+        //        }
+
+        //        Console.WriteLine("Ram size : {0}", compmanufacture.RamSize());
+        //        Console.WriteLine("Processor : {0}", compmanufacture.Processor());
+        //        Console.WriteLine("Harddisk : {0}", compmanufacture.HardDiskSize());
+        //        Console.ReadLine();
+
+        //}
+
+        //implemented factory pattern sample code.
+        static void Main()
+        {
+            IcomputerManufacture compmanufacture = computerfacotry.getComputer("Laptop");
+            Console.WriteLine("Ram size : {0}", compmanufacture.RamSize());
+            Console.WriteLine("Processor : {0}", compmanufacture.Processor());
+            Console.WriteLine("Harddisk : {0}", compmanufacture.HardDiskSize());
+            Console.ReadLine();
+        }
+    }
+}
