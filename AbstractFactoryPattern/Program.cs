0992f82028cc2fdb3f2dbf32f109b4bbbef8680e
Janu217 <54596655+Janu217@users.noreply.github.com>
2020-03-29 19:38:35 +0530

    Add files via upload

---
+++ AbstractFactoryPattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IComputerManufacture factory = new Desktop();// Or new Laptop();
+            ComputerShop shop = new ComputerShop(factory);
+            shop.AssembleMachine();
+            Console.ReadLine();
+        }
+    }
+}
